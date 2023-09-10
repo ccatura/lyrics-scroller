1a8e6ab708a4de721a2af554ebc08d4eee89356e
ccatura <charlie@charliecatura.com>
2023-09-10 09:32:52 -0400

    fixed update query

--- save_song.php
+++ save_song.php
@@ -23,19 +23,15 @@ $data = json_decode(file_get_contents("php://input"), true);
 
 $song_array = json_decode($data, true);
 $user_name  = $_SESSION['user_name'];
-$id         = $song_array['id'];
+$id         = (int)$song_array['id'];
 $title      = $song_array['title'];
 $sub_title  = $song_array['sub_title'];
 $lyrics     = $song_array['lyrics'];
 $message    = $song_array['message'];
-// echo ($user_name);
+echo "{$user_name} {$id} {$title} {$sub_title}";
 
-if ($id != '') {
+if ($id != 0) {
     $result = mysqli_query($conn,  "UPDATE `songs` SET title = '{$title}', sub_title = '{$sub_title}', lyrics = '{$lyrics}' WHERE `id` = '{$id}';");
-
-    
-
-
 } else {
     $result = mysqli_query($conn,  "REPLACE INTO `songs` (`user_name`, `title`, `sub_title`, `lyrics`) VALUES ('{$user_name}','{$title}','{$sub_title}','{$lyrics}');");
 }
